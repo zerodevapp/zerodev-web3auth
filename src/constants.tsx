8e39648816d75afce876c682fb1edc9eecc411c2
Zobeir Hamid <zobeirhamid@berkeley.edu>
2023-08-03 20:16:58 -0700

    feat: adds base

--- src/constants.tsx
+++ src/constants.tsx
@@ -26,7 +26,9 @@ export const CHAIN_ID_TO_NODE: { [key: number]: string} = {
   1313161555: `https://aurora-testnet.infura.io/v3/${INFURA_API_KEY}`,
   56: 'https://wandering-quaint-reel.bsc.quiknode.pro/508c3d245c14adb8689ed4073d29aa5795dfa24e/',
   97: 'https://sly-indulgent-paper.bsc-testnet.quiknode.pro/ab7e00c229f5967334160958e40fd6a4d893fb93/',
-  84531: 'https://icy-long-mountain.base-goerli.quiknode.pro/5b80d93e97cc9412a63c10a30841869abbef9596/'
-
+  8453: 'https://twilight-red-tree.base-mainnet.quiknode.pro/dc6eb27bf0f917df215922488dd97f4de7d9b08e/',
+  84531: 'https://icy-long-mountain.base-goerli.quiknode.pro/5b80d93e97cc9412a63c10a30841869abbef9596/',
+  100: 'https://nd-102-326-870.p2pify.com/3a2549e32e81ed7757e597dcec9922f3',
+  10200: 'https://nd-810-853-201.p2pify.com/e828b09f0d43591de96c297b3f36fffd'
 }
 export const ZERODEV_CLIENT_ID = process.env.REACT_APP_ZEROKIT_WEB3AUTH_CLIENT_ID ?? 'BEjNZMt6TPboj3TfHM06MP8Yxz7cKQX6eK3KZzVhrIMi7jALcZHxJv5o3fDLM7EL4QfPlf2AV_qe155vyR3QxiU'
