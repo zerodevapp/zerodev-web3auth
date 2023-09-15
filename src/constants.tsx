ad0a33394e8cef5cdc91db43e3bb3b71d0433161
Zobeir Hamid <zobeirhamid@berkeley.edu>
2023-09-14 17:44:58 -0700

    fix: rpc base

--- src/constants.tsx
+++ src/constants.tsx
@@ -16,21 +16,24 @@ export const INFURA_API_KEY = 'f36f7f706a58477884ce6fe89165666c'
 export const CHAIN_ID_TO_NODE: { [key: number]: string} = {
   1: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
   5: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
-  11155111: 'https://fittest-ultra-aura.ethereum-sepolia.quiknode.pro/3893d01b1dd411fdfa9b6dd372dd2b4f69fcf1ea/',
+  11155111:
+    "https://fittest-ultra-aura.ethereum-sepolia.quiknode.pro/3893d01b1dd411fdfa9b6dd372dd2b4f69fcf1ea/",
   137: `https://polygon-mainnet.infura.io/v3/${INFURA_API_KEY}`,
   80001: `https://polygon-mumbai.infura.io/v3/${INFURA_API_KEY}`,
   10: `https://optimism-mainnet.infura.io/v3/${INFURA_API_KEY}`,
   420: `https://optimism-goerli.infura.io/v3/${INFURA_API_KEY}`,
   42161: `https://arbitrum-mainnet.infura.io/v3/${INFURA_API_KEY}`,
+  // 42161: 'https://evocative-stylish-dinghy.arbitrum-mainnet.discover.quiknode.pro/80b526d14fa9fd9a8b0db1e65554acaf00c6a1ab/',
   421613: `https://arbitrum-goerli.infura.io/v3/${INFURA_API_KEY}`,
   43114: `https://avalanche-mainnet.infura.io/v3/${INFURA_API_KEY}`,
   43113: `https://avalanche-fuji.infura.io/v3/${INFURA_API_KEY}`,
   1313161554: `https://aurora-mainnet.infura.io/v3/${INFURA_API_KEY}`,
   1313161555: `https://aurora-testnet.infura.io/v3/${INFURA_API_KEY}`,
-  56: 'https://wandering-quaint-reel.bsc.quiknode.pro/508c3d245c14adb8689ed4073d29aa5795dfa24e/',
-  97: 'https://sly-indulgent-paper.bsc-testnet.quiknode.pro/ab7e00c229f5967334160958e40fd6a4d893fb93/',
-  84531: 'https://icy-long-mountain.base-goerli.quiknode.pro/5b80d93e97cc9412a63c10a30841869abbef9596/',
-  100: 'https://nd-102-326-870.p2pify.com/3a2549e32e81ed7757e597dcec9922f3',
-  10200: 'https://nd-810-853-201.p2pify.com/e828b09f0d43591de96c297b3f36fffd'
+  56: "https://neat-greatest-layer.bsc.quiknode.pro/9405a499ceee314e5f2f68c9d47518d3537fce6a/",
+  8453: "https://twilight-red-tree.base-mainnet.quiknode.pro/dc6eb27bf0f917df215922488dd97f4de7d9b08e/",
+  84531:
+    "https://icy-long-mountain.base-goerli.quiknode.pro/5b80d93e97cc9412a63c10a30841869abbef9596/",
+  100: "https://thrilling-fluent-film.xdai.quiknode.pro/305955cffb9868cdd95b5e3dc9775f20678ad9ac/",
+  10200: "https://nd-810-853-201.p2pify.com/e828b09f0d43591de96c297b3f36fffd",
 }
 export const ZERODEV_CLIENT_ID = process.env.REACT_APP_ZEROKIT_WEB3AUTH_CLIENT_ID ?? 'BEjNZMt6TPboj3TfHM06MP8Yxz7cKQX6eK3KZzVhrIMi7jALcZHxJv5o3fDLM7EL4QfPlf2AV_qe155vyR3QxiU'
